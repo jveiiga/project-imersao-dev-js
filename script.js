91dc329e86d987dd9f97c3d27b5151231bb76269
Jeferson Veiga <jeferson.veiiga@icloud.com>
2022-03-21 21:24:54 -0300

    new-features

--- script.js
+++ script.js
@@ -1,21 +1,55 @@
-function adicionarFilme() {
-  var filmeFavorito = document.getElementById("filme").value;
-  var erroFilme = document.getElementById("mensagemDeErro").value;
-  if (filmeFavorito.endsWith(".jpg") ||
-      filmeFavorito.endsWith(".jpeg")||
-      filmeFavorito.endsWith(".png")) {
-    listarFilmeNaTela(filmeFavorito);
-  } else {
-      console.error("Ás imagens devem conter as extensões JPEG, JPG ou PNG");
+var jeferson = {nome: "Jeferson", vitorias: 1, empates: 2, derrotas: 4, pontos: 0};
+var neuza = {nome: "Neuza", vitorias: 3, empates: 0, derrotas: 0, pontos: 0};
+var gleiber = {nome: "Gleiber", vitorias: 1, empates: 2, derrotas: 4, pontos: 0};
+
+function calcularPontos(jogador) {
+  var pontos = jogador.vitorias * 3 + jogador.empates;
+  return pontos;
+}
+
+jeferson.pontos = calcularPontos(jeferson);
+neuza.pontos = calcularPontos(neuza);
+gleiber.pontos = calcularPontos(gleiber);
+
+var jogadores = [jeferson, neuza, gleiber];
+
+function exibirJogadorNaTela(jogadores) {
+  var elemento = ""
+  for(var i = 0; i < jogadores.length; i++) {  
+    elemento += "<tr><td>" + jogadores[i].nome + "</td>"
+    elemento += "<td>" + jogadores[i].vitorias + "</td>"
+    elemento += "<td>" + jogadores[i].empates + "</td>"
+    elemento += "<td>" + jogadores[i].derrotas +"</td>"
+    elemento += "<td>" + jogadores[i].pontos + "</td>"
+    elemento += "<td><button onClick='adicionarVitoria(" + i + ")'>Vitória</button></td>"
+    elemento += "<td><button onClick='adicionarEmpate(" + i + ")'>Empate</button></td>"
+    elemento += "<td><button onClick='adicionarDerrota(" + i + ")'>Derrota</button></td>"
+    elemento += "</tr>"
   }
-  document.getElementById("filme").value = "";
+  
+  var tabelaJogadores = document.getElementById("tabelaJogadores");
+  tabelaJogadores.innerHTML = elemento;
+  
 }
 
-function listarFilmeNaTela(filme) {
-  console.log(filme);
-  var elementoFilmeFavorito = "<img src=" + filme + ">";
-  var elementoListaFilmes = document.getElementById("listaFilmes");
-  elementoListaFilmes.innerHTML = elementoListaFilmes.innerHTML + elementoFilmeFavorito;
+exibirJogadorNaTela(jogadores)
+
+function adicionarVitoria(i) {
+  var jogador = jogadores[i];
+  jogador.vitorias++;
+  jogador.pontos = calcularPontos(jogador);
+  exibirJogadorNaTela(jogadores);
 }
 
-//erroFilme.innerHTML = "Ás imagens devem conter as extensões JPEG, JPG ou PNG";
+function adicionarEmpate(i) {
+  var jogador = jogadores[i];
+  jogador.empates++
+  jogador.pontos = calcularPontos(jogador);
+  exibirJogadorNaTela(jogadores);
+}
+
+function adicionarDerrota(i) {
+  var jogador = jogadores[i];
+  jogador.derrotas++
+  exibirJogadorNaTela(jogadores);
+}
